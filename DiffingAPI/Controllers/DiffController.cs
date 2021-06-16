ee03973959e21d909514f78c814c088bc57a234e
Rok Galin <galinrok1@gmail.com>
2021-06-16 22:12:42 +0200

    Add project files.

---
+++ DiffingAPI/Controllers/DiffController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiffingAPI.Controllers
+{
+    public enum side
+    {
+        left = 0,
+        right
+    }
+    [ApiController]
+    public class DiffController : ControllerBase
+    {
+        /// <summary>
+        /// return data za določen ID in Stran
+        /// </summary>
+        /// <param name="id">ID vnosa</param>
+        /// <param name="side">Stran vnosa</param>
+        /// <returns></returns>
+        [Route("/v1/diff/{id}/{side}")]
+        public IActionResult Get(int id, side side)
+        {
+                return Ok($"returning value for id: {id} on {side} side");
+        }
+
+        /// <summary>
+        /// Save data(base64) za določen ID in stran 
+        /// </summary>
+        /// <param name="id">id vnosa</param>
+        /// <param name="side">stran vnosa</param>
+        /// <param name="base64">Base64 string</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("/v1/diff/{id}/{side}")]
+        public IActionResult Put(int id, side side, string base64)
+        {
+
+            return StatusCode(201);
+        }
+
+
+    }
+}
---
+++ DiffingLibrary/Models/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiffingLibrary.Models
+{
+    public class Data
+    {
+        private string _base;
+
+        public string MyProperty
+        {
+            get { return _base; }
+            set { _base = value; }
+        }
+
+    }
+}
